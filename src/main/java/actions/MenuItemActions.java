f6b305d898962823c1ab684b713ce0feebc06f1b
ramuntech <ramuntech@gmail.com>
2022-06-13 11:50:28 +0530

    update menuitempage

--- src/main/java/actions/MenuItemActions.java
+++ src/main/java/actions/MenuItemActions.java
@@ -1,12 +1,17 @@
 package actions;
 
 import core.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import pages.CreateMenuItemPage;
+import pages.MenuItemPage;
 import utilities.ReportLog;
 
-public class MenuItemActions extends CreateMenuItemPage {
+import java.util.List;
+
+public class MenuItemActions extends MenuItemPage {
 
 
     public MenuItemActions(WebDriver driver) {
@@ -17,6 +22,14 @@ public class MenuItemActions extends CreateMenuItemPage {
         String message = BasePage.SuccesMsg(getDriver(), "Menu Item  success message");
         Assert.assertTrue(message.equalsIgnoreCase("MenuItem Inserted Successfully"), "Success message is not matched");
         ReportLog.PASS("Menu Item inserted successfully message validated Successfully");
+    }
+
+    public void VerifyNameInTheList()
+    {
+        String locator = String.format(titleColumnData, CreateMenuItemActions.title);
+        WebElement title = getElement(By.xpath(locator), "New Title");
+        Assert.assertNotNull(title, "Title is available in the list");
+        ReportLog.PASS("Title is verified in the list successfully");
     }
 
 
